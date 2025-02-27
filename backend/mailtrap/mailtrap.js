c5948a0d1c7f653ef3f8f13e74b97dcbf0d01c5a
Bublaaa <yanuarkrisna7@gmail.com>
2025-02-27 21:04:23 +0700

    Add bycrypt and email template

---
+++ backend/mailtrap/mailtrap.js
@@ -0,0 +1,29 @@
+import { MailtrapClient } from "mailtrap";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const mailtrapClient = new MailtrapClient({
+  // endpoint: process.end.MAILTRAP_ENDPOINT,
+  token: process.env.MAILTRAP_TOKEN,
+});
+
+export const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Coffeshier",
+};
+// const recipients = [
+//   {
+//     email: "yanuarkrisna7@gmail.com",
+//   }
+// ];
+
+// client
+//   .send({
+//     from: sender,
+//     to: recipients,
+//     subject: "You are awesome!",
+//     html: "Congrats for sending test email with Mailtrap!",
+//     category: "Integration Test",
+//   })
+//   .then(console.log, console.error);
